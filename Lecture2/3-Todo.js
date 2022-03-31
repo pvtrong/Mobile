e338d91c6ed9a6f5b43381494e72a4f2cb2e2574
pvtrong <phamvantrong.va@gmail.com>
2022-03-31 22:06:40 +0700

    Lecture2 and Project0

---
+++ Lecture2/3-Todo.js
@@ -0,0 +1,16 @@
+class Todo {
+    constructor(configuration) {
+      this.text = configuration.text || 'New TODO'
+      this.checked = false
+    }
+  
+    render() {
+      return (
+        <li>
+          <input type="checkbox" checked={this.checked} />
+          <span>{this.text}</span>
+        </li>
+      )
+    }
+  }
+  
