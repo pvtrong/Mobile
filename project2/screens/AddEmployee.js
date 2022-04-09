db3c0bf9348f0fd925398514fe23e6fe9699cb47
pvtrong <phamvantrong.va@gmail.com>
2022-04-09 22:46:06 +0700

    feat: Them nhan vien

--- project2/screens/AddEmployee.js
+++ project2/screens/AddEmployee.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, FlatList, Button, StyleSheet, Picker } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, FlatList, Button, StyleSheet } from "react-native";
 import axios from "axios";
 import EmployeeListItem from "../components/EmployeeListItem";
 import { PagingEnum } from "../commons/enums/paging.enum";
@@ -9,185 +9,177 @@ import { TextInput } from "react-native-gesture-handler";
 import { ModeEnum } from "../commons/enums/mode.enum";
 import Toast from "react-native-toast-message";
 import { GenderConstant } from "../commons/constants/gender.constant";
+import { Picker } from "@react-native-picker/picker";
 import { GenderEnum } from "../commons/enums/gender.enum";
-export default class AddEmployee extends React.Component {
-	constructor(props) {
-		super(props);
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { useFocusEffect } from '@react-navigation/native';
+export default function AddEmployee(props) {
+	const [employeeCode, setEmployeeCode] = useState(null);
+	const [employeeName, setEmployeeName] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("088888888");
+	const [email, setEmail] = useState("a@gmail.com");
+	const [gender, setGender] = useState(GenderEnum.male);
+	const [genderName, setGenderName] = useState("Nam");
+	const [birthday, setBirthday] = useState(new Date());
+	const [genders, setGenders] = useState([
+		{
+			label: "Nam",
+			value: GenderEnum.male,
+		},
+		{
+			label: "Nữ",
+			value: GenderEnum.female,
+		},
+		{
+			label: "Khác",
+			value: GenderEnum.other,
+		},
+	]);
+
+    useFocusEffect(
+        React.useCallback(() => {
+          const unsubscribe = async function(){
+            try {
+                var res = await axios.get(
+                    `${Config.BaseUrl}/api/v1/Employees/NewEmployeeCode`
+                );
+                setEmployeeCode(res.data);
+                console.log("data2", res.data);
+            } catch (err) {
+                console.log(err);
+                
+            }
+          }
+    
+          return () => unsubscribe();
+        })
+      );
+	useEffect(async () => {
+		let mounted = true;
 		props.navigation.setOptions({
 			title: "Thêm nhân viên mới",
 		});
-		this.state = {
-			employee: {},
-			genders: [
-				{
-					label: "Nam",
-					value: GenderEnum.male,
-				},
-				{
-					label: "Nữ",
-					value: GenderEnum.female,
-				},
-				{
-					label: "Khác",
-					value: GenderEnum.other,
-				},
-			],
-		};
-	}
-
-	handleEmployeeCode = (employeeCode) => {
-		this.setState((prevState) => {
-			let employee = Object.assign({}, prevState.employee);
-			employee.EmployeeCode = employeeCode;
-			return { employee };
-		});
-	};
-	handleEmployeeName = (employeeName) => {
-		this.setState((prevState) => {
-			let employee = Object.assign({}, prevState.employee);
-			employee.EmployeeName = employeeName;
-			return { employee };
-		});
-		console.log(this.state.employee);
-	};
-	handlePhoneNumber = (phoneNumber) => {
-		this.setState((prevState) => {
-			let employee = Object.assign({}, prevState.employee);
-			employee.PhoneNumber = phoneNumber;
-			return { employee };
-		});
-		console.log(this.state.employee);
-	};
-	handleEmail = (email) => {
-		this.setState((prevState) => {
-			let employee = Object.assign({}, prevState.employee);
-			employee.Email = email;
-			return { employee };
-		});
-		console.log(this.state.employee);
-	};
-	handleGender = (gender) => {
-		this.setState((prevState) => {
-			let employee = Object.assign({}, prevState.employee);
-			switch (gender) {
-				case GenderEnum.male: {
-					employee.GenderName = "Nam";
-					break;
-				}
-				case GenderEnum.female: {
-					employee.GenderName = "Nữ";
-					break;
-				}
-				default: {
-					employee.GenderName = "Khác";
-					break;
-				}
-			}
-			employee.Gender = gender;
-			return { employee };
-		});
-	};
-
-	showToast() {
-		Toast.show({
-			type: "success",
-			text1: "Hello",
-		});
-	}
-
-	render() {
-		const { route, navigation } = this.props;
-		let self = this;
-		const { employee, genders } = this.state;
-		return (
-			<View style={styles.container}>
-				<View style={styles.formControl}>
-					<Text style={styles.label}>Mã nhân viên</Text>
-					<TextInput
-						style={styles.input}
-						placeholder="Nhập mã nhân viên"
-						value={employee.EmployeeCode}
-						onChangeText={self.handleEmployeeCode}
-						editable={false}
-					></TextInput>
-				</View>
-
-				<View style={styles.formControl}>
-					<Text style={styles.label}>Họ và tên</Text>
-					<TextInput
-						autoFocus={true}
-						style={styles.input}
-						value={employee.EmployeeName}
-						onChangeText={self.handleEmployeeName}
-						placeholder="Nhập họ và tên"
-					></TextInput>
-				</View>
-				<View style={styles.formControl}>
-					<Text style={styles.label}>Giới tính</Text>
-
-					<View style={styles.select}>
-                    <Picker
-						selectedValue={employee.Gender}
-						onValueChange={(itemValue, itemIndex) => {self.handleGender(itemValue)}}
-                        style={{ fontWeight: 'bold'}}
-					>
-						{genders.map((x, index) => (
-							<Picker.Item key={index} label={x.label} value={x.value} />
-						))}
-					</Picker>
-                    </View>
-				</View>
-                <View style={styles.formControl}>
-					<Text style={styles.label}>Giới tính</Text>
-
-					<View style={styles.select}>
-                            
-                    </View>
-				</View>
-                
-				<View style={styles.formControl}>
-					<Text style={styles.label}>Số điện thoại</Text>
-					<TextInput
-						style={styles.input}
-						value={employee.PhoneNumber}
-						onChangeText={self.handlePhoneNumber}
-						placeholder="Nhập số điện thoại"
-					></TextInput>
-				</View>
-				<View style={styles.formControl}>
-					<Text style={styles.label}>Email</Text>
-					<TextInput
-						style={styles.input}
-						value={employee.Email}
-						onChangeText={self.handleEmail}
-						placeholder="Nhập email"
-					></TextInput>
-				</View>
-				<Button title="Thêm" onPress={this.showToast}></Button>
-			</View>
-		);
-	}
-
-	async componentDidMount() {
-		let self = this;
-		console.log(this);
-		switch (self.props.route.params.mode) {
+		// Update the document title using the browser API
+		switch (props.route.params.mode) {
 			case ModeEnum.add: {
 				try {
 					var res = await axios.get(
 						`${Config.BaseUrl}/api/v1/Employees/NewEmployeeCode`
 					);
-					const employee = {};
-					employee.EmployeeCode = res.data;
-					self.setState(() => {
-						return { employee: employee };
-					});
+					if (mounted) {
+						setEmployeeCode(res.data);
+					}
+                    console.log("data1", res.data);
 				} catch (err) {
 					console.log(err);
 				}
 			}
 		}
-	}
+		return () => (mounted = false);
+	}, [props.route.params]);
+
+    let addEmployee = async function() {
+        let employee = {
+            employeeCode: employeeCode,
+            employeeName: employeeName,
+            email: email,
+            phoneNumber: phoneNumber,
+            gender: gender,
+            genderName: genderName,
+            departmentName: props.route.params.department.departmentName,
+            departmentId: props.route.params.department.departmentId,
+        }
+        console.log(employee)
+        try {
+            var res = await axios.post(
+                `${Config.BaseUrl}/api/v1/Employees`, employee
+            );
+
+            console.log(res.error)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+	return (
+		<View style={styles.container}>
+			<View style={styles.formControl}>
+				<Text style={styles.label}>Mã nhân viên</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Nhập mã nhân viên"
+					value={employeeCode}
+					onChangeText={(text) => setEmployeeCode(text)}
+					editable={false}
+				></TextInput>
+			</View>
+			<View style={styles.formControl}>
+				<Text style={styles.label}>Họ và tên</Text>
+				<TextInput
+					autoFocus={true}
+					style={styles.input}
+					value={employeeName}
+					onChangeText={(text) => setEmployeeName(text)}
+					placeholder="Nhập họ và tên"
+				></TextInput>
+			</View>
+			<View style={styles.formControl}>
+				<Text style={styles.label}>Giới tính</Text>
+
+				<View style={styles.select}>
+					<Picker
+						selectedValue={gender}
+						onValueChange={(itemValue, itemIndex) => {
+							setGender(itemValue);
+                            setGenderName(genders[itemIndex].label)
+						}}
+						style={{ fontWeight: "bold" }}
+					>
+						{genders.map((x, index) => (
+							<Picker.Item key={index} label={x.label} value={x.value} />
+						))}
+					</Picker>
+				</View>
+			</View>
+			{/* <View style={styles.formControl}>
+				<Text style={styles.label}>Ngày sinh</Text>
+				<View style={styles.select}>
+					<Text style={styles.label}>{new Date(birthday).toString()}</Text>
+				</View>
+				<RNDateTimePicker
+					mode="date"
+					onChange={(value) => {
+						setBirthday(new Date(value.nativeEvent.timestamp));
+					}}
+					value={birthday}
+				/>
+			</View> */}
+
+			<View style={styles.formControl}>
+				<Text style={styles.label}>Số điện thoại</Text>
+				<TextInput
+					style={styles.input}
+					value={phoneNumber}
+					onChangeText={(text) => setPhoneNumber(text)}
+					placeholder="Nhập số điện thoại"
+				></TextInput>
+			</View>
+			<View style={styles.formControl}>
+				<Text style={styles.label}>Email</Text>
+				<TextInput
+					style={styles.input}
+					value={email}
+					onChangeText={(text) => setEmail(text)}
+					placeholder="Nhập email"
+				></TextInput>
+			</View>
+			<Button
+				title="Thêm"
+				onPress={() => addEmployee()
+				}
+			></Button>
+			{/* <Button title="Thêm" ></Button> */}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
@@ -207,7 +199,6 @@ const styles = StyleSheet.create({
 		marginRight: 16,
 		height: 48,
 		padding: 16,
-        
 	},
 	label: {
 		color: "#223263",
@@ -224,7 +215,6 @@ const styles = StyleSheet.create({
 		marginRight: 16,
 		borderColor: "#EBF0FF",
 		borderWidth: 1,
-        borderRadius: 4,
-        
+		borderRadius: 4,
 	},
 });
--- project2/screens/Category.js
+++ project2/screens/Category.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, Text, FlatList, Button } from "react-native";
 import axios from "axios";
 import EmployeeListItem from "../components/EmployeeListItem";
@@ -6,67 +6,81 @@ import { PagingEnum } from "../commons/enums/paging.enum";
 import Convert from "../commons/utils/convert";
 import { Config } from "../config/config";
 import { ModeEnum } from "../commons/enums/mode.enum";
+import { useFocusEffect } from '@react-navigation/native';
 
-export default class Category extends React.Component {
-	constructor(props) {
-		super(props);
+export default function Category(props) {
+	const [employees, setEmployees] = useState([]);
+	const [pageSize, setPageSize] = useState(PagingEnum.first);
+	const [pageNumber, setPageNumber] = useState(1);
+	const [departmentId, setDepartmentId] = useState(
+		props.route.params.departmentId
+	);
+    
+    useFocusEffect(
+        React.useCallback(() => {
+          const unsubscribe = async function(){
+            try {
+                var queryStr = Convert.objectToQueryString({pageSize: pageSize, pageNumber: pageNumber, departmentId: departmentId});
+                var res = await axios.get(
+                    `${Config.BaseUrl}/api/v1/Employees/filter?${queryStr}`
+                );
+                const employees = res.data.Data;
+                setEmployees(employees);
+            } catch (err) {
+                console.log(err);
+            }
+          }
+    
+          return () => unsubscribe();
+        },)
+      );
+	useEffect(async () => {
+		let mounted = true;
 		props.navigation.setOptions({
 			title: props.route.params.name,
 		});
-		this.state = {
-			employees: [],
-			filter: {
-				pageSize: PagingEnum.first,
-				pageNumber: 1,
-				departmentId: props.route.params.departmentId,
-			},
-		};
-	}
-	render() {
-		const { route, navigation } = this.props;
-		const { employees } = this.state;
-		return (
-			<View>
-				<Button
-					title={"Thêm nhân viên"}
-					onPress={() => navigation.navigate("AddEmployee", {mode: ModeEnum.add})}
-				></Button>
-				<FlatList
-					data={employees}
-					renderItem={({ item }) => (
-						<EmployeeListItem
-							employee={item}
-							onPress={() =>
-								navigation.navigate("Employee", { employee: item })
-							}
-						/>
-					)}
-					keyExtractor={(item) => `${item.EmployeeId}`}
-					contentContainerStyle={{
-						paddingLeft: 16,
-						paddingRight: 16,
-					}}
-				></FlatList>
-			</View>
-		);
-	}
-
-	async componentDidMount() {
+		// Update the document title using the browser API
 		let self = this;
 		try {
-			const { filter } = this.state;
-			var queryStr = Convert.objectToQueryString(filter);
+			var queryStr = Convert.objectToQueryString({pageSize: pageSize, pageNumber: pageNumber, departmentId: departmentId});
 			var res = await axios.get(
 				`${Config.BaseUrl}/api/v1/Employees/filter?${queryStr}`
-				// `${Config.BaseUrl}/api/v1/Employees`
 			);
-			// console.log(res.data.Data)
 			const employees = res.data.Data;
-			self.setState(() => {
-				return { employees: employees };
-			});
+			setEmployees(employees);
 		} catch (err) {
 			console.log(err);
 		}
-	}
+		return () => (mounted = false);
+	});
+	return (
+		<View>
+			<Button
+				title={"Thêm nhân viên"}
+				onPress={() =>
+					props.navigation.navigate("AddEmployee", {
+						mode: ModeEnum.add,
+						department: {
+							departmentName: props.route.params.name,
+							departmentId: departmentId,
+						},
+					})
+				}
+			></Button>
+			<FlatList
+				data={employees}
+				renderItem={({ item }) => (
+					<EmployeeListItem
+						employee={item}
+						onPress={() => props.navigation.navigate("Employee", { employee: item })}
+					/>
+				)}
+				keyExtractor={(item) => `${item.EmployeeId}`}
+				contentContainerStyle={{
+					paddingLeft: 16,
+					paddingRight: 16,
+				}}
+			></FlatList>
+		</View>
+	);
 }
